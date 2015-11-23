9667fe2bf035655c7b5bd58bf2a6d9dc6099d053
Sameid <sameid.usmani@gmail.com>
2015-11-23 13:40:15 -0500

    Seeds fix

--- src/simModel/Seeds.java
+++ src/simModel/Seeds.java
@@ -4,7 +4,9 @@ import cern.jet.random.engine.RandomSeedGenerator;
 
 public class Seeds 
 {
-	int[] seeds = new int[10];
+	
+	
+	int[] seeds = new int[8];
 	int index = 0;
 
 	public Seeds(RandomSeedGenerator rsg)
@@ -14,6 +16,7 @@ public class Seeds
 	}
 	
 	public int getSeed(){
+		if (index >= 8) index = 0;
 		int r = seeds[index];
 		index++;
 		return r;
